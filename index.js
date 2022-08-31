191723a1ed7072a6272ac8214689af84e0e42923
Vinícius Soares <vinivsp2304@hotmail.com>
2022-08-30 21:02:46 -0300

    criadas rotas: post e get

--- index.js
+++ index.js
@@ -17,3 +17,47 @@ app.listen(2356, () => {
 });
 app.use(json());
 app.use(cors());
+
+// ## Rotas ##
+
+app.get("/games", async (req, res) => {
+  const response = await Games.findAll({
+    order: [["createdAt", "DESC"]],
+  });
+  res.send(response);
+});
+
+app.post("/post/game", async (req, res) => {
+  const { title, price, year } = req.body;
+  // if (!title && !price && !year) {
+  //   return res.status(400).json({
+  //     causa: "Nenhum dos campos podem ser vazios",
+  //     solucao: "Preencha os campos corretamente",
+  //   });
+  // }
+
+  // if (!title) {
+  // }
+  // if (!price) {
+  // }
+  // if (!year) {
+  // }
+
+  const reqField = ["title", "price", "year"];
+
+  for (const field of reqField) {
+    if (!req.body[field]) {
+      return res.status(400).json({
+        causa: "Nenhum dos campos podem ser vazios",
+        solucao: "Preencha todos os campos corretamente",
+      });
+    }
+  }
+
+  await Games.create({
+    title: title,
+    price: price,
+    year: year,
+  });
+  res.send("Jogo salvo com sucesso.");
+});
